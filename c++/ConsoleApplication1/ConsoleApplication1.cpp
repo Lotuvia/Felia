2bd10492e69cda0648125152a097c39713f40436
Lotuvia <egogas219349@gmail.com>
2021-03-27 16:52:42 +0300

    seventh commit

--- c++/ConsoleApplication1/ConsoleApplication1.cpp
+++ c++/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,7 @@
 ﻿
 #include <iostream>
+#include <stdio.h>
+#include <time.h>
 
 int main()
 {
@@ -27,8 +29,20 @@ int main()
         x = x * 100000;
     }
     std::cout << "random number(0,9999) after filter = " << x << std::endl;
+    int tm_year;
+    time_t time(time_t * time);
+    char* ctime(const time_t * time);
+    struct tm* localtime(const time_t*time);
+    std::cout << "time.1 = " << time << std::endl;
+    std::cout << "time.2 = " << ctime << std::endl;
+    std::cout << "time.3 = " << localtime << std::endl;
+    int code = x;
 
-   
-
+    struct tm* ptr;
+    time_t It;
+    It = time(NULL);
+    ptr = localtime(&It);
+    printf(asctime(ptr));
+  
     return 0;
 }

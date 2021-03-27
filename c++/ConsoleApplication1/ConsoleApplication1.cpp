bf1284c17f1ba6a28cc2a539e982558b113644c1
Lotuvia <egogas219349@gmail.com>
2021-03-27 17:05:15 +0300

    eighth commit

--- c++/ConsoleApplication1/ConsoleApplication1.cpp
+++ c++/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,7 +1,9 @@
-﻿
+﻿#include <Windows.h>
 #include <iostream>
 #include <stdio.h>
 #include <time.h>
+using namespace std;
+using std::cout;
 
 int main()
 {
@@ -29,20 +31,22 @@ int main()
         x = x * 100000;
     }
     std::cout << "random number(0,9999) after filter = " << x << std::endl;
+
     int tm_year;
     time_t time(time_t * time);
     char* ctime(const time_t * time);
     struct tm* localtime(const time_t*time);
+
     std::cout << "time.1 = " << time << std::endl;
     std::cout << "time.2 = " << ctime << std::endl;
     std::cout << "time.3 = " << localtime << std::endl;
-    int code = x;
+    
+    SYSTEMTIME st;
+    GetLocalTime(&st);
+    std::cout << "time = " << st.wYear << std::endl;
+    int year = st.wYear % 100;
+    int code = x + year;
+    std::cout << "not unique code = " << code << std::endl;
 
-    struct tm* ptr;
-    time_t It;
-    It = time(NULL);
-    ptr = localtime(&It);
-    printf(asctime(ptr));
-  
     return 0;
 }

8a3888bafd3da1a380c7ec9477628f7d9830a3d6
Sergey Lakeev <sl5564@mail.ru>
2021-03-27 17:14:51 +0300

    Add random,time

--- c++/Hello/Hello.cpp
+++ c++/Hello/Hello.cpp
@@ -1,5 +1,13 @@
-#include<stdio.h>
+#include<iostream>
+#include<stdlib>
+#include<ctime>
 int main(){
-printf("Hello\n");
+randomize();
+int a= 1+ random(10000), b;
+std::cout<<"Random value: "<<a<<std::endl;
+time_t now = time(0);
+char* dt = ctime(&now);
+std::cout<<dt;
+system("pause");
 return 0;
 }

ad98f7d5e97a07b827a677e3bd389eca6bd083d6
Lotuvia <egogas219349@gmail.com>
2021-03-27 17:51:58 +0300

    popytka sdelat' massive

--- c++/ConsoleApplication1/ConsoleApplication1.cpp
+++ c++/ConsoleApplication1/ConsoleApplication1.cpp
@@ -11,6 +11,9 @@ int main()
     std::cout << "RAND_MAX = " << RAND_MAX << std::endl;
     std::cout << "random number = " << rand() << std::endl;
 
+#pragma region массив
+
+    int box[100] = {};
     int x = 1 + rand() % 9999;
     std::cout << "random number(0,9999) = " << x << std::endl;
 
@@ -30,23 +33,58 @@ int main()
     {
         x = x * 100000;
     }
+    
+    for (int i = 0; i < 100; i++)
+
+    {
+        int j = 0;
+
+
+        if (box[j] == 0)
+        {
+        }
+        else
+        {
+            std::cout << "massive " << box[j] << std::endl;
+        }
+        j++;
+    }
+
+#pragma endregion
+
+  /*  int x = 1 + rand() % 9999;
+    std::cout << "random number(0,9999) = " << x << std::endl;
+
+    if (x / 10000 == 0)
+    {
+        x = x * 100;
+    }
+    else if (x / 1000 == 0)
+    {
+        x = x * 1000;
+    }
+    else if (x / 100 == 0)
+    {
+        x = x * 10000;
+    }
+    else if (x / 10 == 0)
+    {
+        x = x * 100000;
+    }
+    */
+
     std::cout << "random number(0,9999) after filter = " << x << std::endl;
 
-    int tm_year;
-    time_t time(time_t * time);
-    char* ctime(const time_t * time);
-    struct tm* localtime(const time_t*time);
-
-    std::cout << "time.1 = " << time << std::endl;
-    std::cout << "time.2 = " << ctime << std::endl;
-    std::cout << "time.3 = " << localtime << std::endl;
-    
     SYSTEMTIME st;
     GetLocalTime(&st);
+
     std::cout << "time = " << st.wYear << std::endl;
+
     int year = st.wYear % 100;
     int code = x + year;
+
     std::cout << "not unique code = " << code << std::endl;
+
     cin.get();
     system("PAUSE");
     return 0;
